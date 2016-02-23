4b7c7043911ee9e005b1b62e4854b47dd97bd89a
Pav <pbuderaski@gmail.com>
2016-02-23 17:04:19 +0000

    Added login errors for user

--- ng/login.ctrl.js
+++ ng/login.ctrl.js
@@ -3,8 +3,11 @@ angular.module('app')
 	$scope.login = function(username, password) {
 		UserSvc.login(username, password)
 			.then(function(response) {
+				delete $scope.loginError
 				$scope.$emit('login', response.data)
 				$location.path('/')
+			}, function () {
+				$scope.loginError = "Wrong username or password"
 			})
 	}
 }])
