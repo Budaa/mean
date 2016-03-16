46f8f3ac82d06c9f1d45095dd0aedc19f34b645f
Pav <pbuderaski@gmail.com>
2016-03-16 14:44:02 +0000

    setting mocha

---
+++ test/server/controllers/api/posts.spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+var ctrl = require('../../../../controllers/api/posts')
+
+describe('controllers.api.posts', function() {
+	it('exist', function() {
+		expect(ctrl).to.exist
+	})
+})
